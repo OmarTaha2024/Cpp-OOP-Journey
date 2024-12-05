27245cc2d3f065b7ae86e75fb2c4d54363fe397e
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2024-12-06 00:03:27 +0200

    Implement single inheritance example with Vehicle as base class and Car as derived class and ElectricCar  as derived class from Car

---
+++ Inheritance/Single Inheritance/main.cpp
@@ -0,0 +1,92 @@
+/**                         Documentation section
+_________________________________________________________________________________________________________________
+-----------------------------------------------------------------------------------------------------------------
+@name :   Single Inheritance
+@author : Omar Mohamed El Sayed Taha
+@Link : ...........................
+
+
+
+__________________________________________________________________________________________________________________
+------------------------------------------------------------------------------------------------------------------
+**/
+#include <bits/stdc++.h>
+#define Taha ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll long long
+#define el "\n"
+#define sp " "
+
+
+
+using namespace std;
+
+
+
+
+/**
+single inheritance :
+class which contain only one base class and only
+one derive class
+**/
+// Base class
+class Vehicle
+{
+protected:
+    string brand;
+    string model;
+
+public:
+    Vehicle(string b, string m) : brand(b), model(m) {}
+
+    void displayInfo() const
+    {
+        cout << "Brand: " << brand << endl;
+        cout << "Model: " << model << endl;
+    }
+};
+
+// Derived class
+class Car : public Vehicle
+{
+private:
+    int doors;
+
+public:
+    Car(string b, string m, int d) : Vehicle(b, m), doors(d) {}
+
+    void displayInfo() const
+    {
+        // Call the base class method
+        Vehicle::displayInfo();
+        cout << "Number of Doors: " << doors << endl;
+    }
+};
+// Another derived class (inherits from Car) but can't access doors because it is private attributes
+class ElectricCar : public Car
+{
+private:
+    int batteryCapacity; // in kWh
+
+public:
+    ElectricCar(string b, string m, int d, int bc)
+        : Car(b, m, d), batteryCapacity(bc) {}
+
+    void displayInfo() const
+    {
+        // Call the parent class method
+        Car::displayInfo();
+        cout << "Battery Capacity: " << batteryCapacity << " kWh" << endl;
+    }
+};
+
+int main()
+{
+    Taha ; // Faster I/O
+    Car myCar("Toyota", "Corolla", 4);
+    myCar.displayInfo();
+
+    ElectricCar myElectricCar("Tesla", "Model S", 4, 100);
+    myElectricCar.displayInfo();
+
+    return 0;
+}
